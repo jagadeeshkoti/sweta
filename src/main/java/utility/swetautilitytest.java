cf3b33c421d11e4d68213359cce16cdf735f0360
jagadeeshkoti <jagadeeshkoti96@gmail.com>
2023-10-02 11:34:12 +0530

    commit1

---
+++ src/main/java/utility/swetautilitytest.java
@@ -0,0 +1,12 @@
+package utility;
+
+import org.testng.annotations.Test;
+
+public class swetautilitytest {
+	@Test
+	public void maximize()
+	{
+		System.out.println("hii");
+	}
+
+}
---
+++ src/test/java/swetatest/demotest.java
@@ -0,0 +1,14 @@
+package swetatest;
+
+import org.testng.annotations.Test;
+
+public class demotest {
+	@Test
+	public void demot()
+	{
+		
+		System.out.println("test");
+	}
+	
+
+}
